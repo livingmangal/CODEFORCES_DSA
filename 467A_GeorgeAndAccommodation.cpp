2802b94654ac28a2f20756ca4ad3786f07b18026
mangalsinghvit <mangalsinghvit@users.noreply.github.com>
2026-08-07 20:00:00 +0530

    Solve 467A - George and Accommodation (C++)

---
+++ 467A_GeorgeAndAccommodation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    if (cin >> n) {
+        int rooms = 0;
+        for (int i = 0; i < n; i++) {
+            int p, q;
+            cin >> p >> q;
+            if (q - p >= 2) rooms++;
+        }
+        cout << rooms << "\n";
+    }
+    return 0;
+}
