e219af9087761c7818d71302cb8a438ebe56b6ad
mangalsinghvit <mangalsinghvit@users.noreply.github.com>
2026-08-27 23:40:09 +0530

    Presents

---
+++ APresents.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class APresents {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] ans = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int p = sc.nextInt();
+
+            // Friend i gave a gift to friend p
+            ans[p] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+
+        sc.close();
+    }
+}
